98cb274ab0830bd7f7fa39e95821fdb8c37f24f2
te <thrzl@icloud.com>
2023-06-20 11:29:50 -0400

    ⚡️ smaller + more accurate model

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import fasttext
 
-model = fasttext.train_supervised('all_categories.data', lr=0.5)
+model = fasttext.train_supervised("all_categories.data", lr=0.5)
+model.quantize("all_categories.data", retrain=True, cutoff=100000)
+model.save_model("quantized_tagged.bin")
 
-model.quantize('all_categories.data', retrain=True)
-
-model.save_model('quantized_tagged.bin')
+print(model.test("valid.data"))
