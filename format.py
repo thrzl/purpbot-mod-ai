230c527eb3d8a90af6e34c49e851185012cda48c
te <thrzl@icloud.com>
2024-06-02 15:00:46 -0400

    🔨 improve scripts

--- format.py
+++ format.py
@@ -1,30 +1,18 @@
-import csv
-from tqdm import tqdm
+from csv import DictReader
 
-COLUMNS = (
-    "id",
-    "comment_text",
-    "toxic",
-    "severe_toxic",
-    "obscene",
-    "threat",
-    "insult",
-    "identity_hate",
-)
-FLAGS = ("toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate")
+FLAG_DICT = {flag: f"__label__{flag}" for flag in ("toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate")}
 
 false_newline = "\\n"
 
+p = True
+with open("train.csv", "r", encoding="utf-8") as csvfile:
+    csv_data = csvfile
+    reader = [i for i in DictReader(csv_data)]
+
 with open("labeled.data", "w", encoding="utf-8", newline="") as f:
-    with open("train.csv", "r", encoding="utf-8") as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in tqdm(reader):
-            labels = [f"__label__{flag}" for flag in FLAGS if row[flag] == "1"]
-            if not labels:
-                labels = ["__label__non_toxic"]
-            text = row["comment_text"].replace("\n", false_newline)
+    for row in reader:
+        labels = (FLAG_DICT[flag] for flag in FLAG_DICT if row[flag] == "1")
+        text = row["comment_text"].replace("\n", false_newline)
 
-            line = f"{' '.join(labels)} {text}"
-            line = line.replace('"', "")
-
-            f.write(line + "\n")
+        line = f"{' '.join(labels) or '__label__non_toxic'} {text}".replace('"', "")
+        f.write(line + '\n')
--- train.py
+++
@@ -1,7 +0,0 @@
-import fasttext
-
-model = fasttext.train_supervised("labeled.data", lr=0.5, epoch=50)
-model.quantize("labeled.data", retrain=True, cutoff=100000)
-model.save_model("model.bin")
-
-print(model.test("valid.data"))
